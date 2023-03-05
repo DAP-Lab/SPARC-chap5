fd02e2cd0f14427de0be194c3d3784b8e9e277bf
Nithya Shikarpur <nithya@Nithyas-MacBook-Air.local>
2023-03-05 13:42:41 -0500

    added duration calc in fig3

--- Code/fig3.py
+++ Code/fig3.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def annotateInteraction(axs, keywords, cs, interactionFile, startTime, duration):
+def annotateInteraction(axs, keywords, cs, interactionFile, startTime=0, duration=None):
     '''Adds interaction annotation to the axes given
 
     Parameters
